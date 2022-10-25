de4c21e565d58c674315b092cee28c0f42132a06
Alejandro <alegartiz@hotmail.com>
2022-10-25 11:31:20 -0300

    final patch

--- js/alerts.js
+++ js/alerts.js
@@ -1,6 +1,6 @@
 Swal.fire({
     title: 'Bienvenido!',
-    html: '<p class="swal">Puedes acceder al repositorio haciendo <br><a href="https://github.com/sn3m0/" target="_blank">click acá</a></p>',
+    html: '<p class="swal">Puedes acceder al repositorio haciendo <br><a href="https://github.com/sn3m0/CGshop01" target="_blank">click acá</a></p>',
     icon: 'info',
     footer: '<span class= pie><i>Desarrollado por <b>Alejandro García</b></i></span>',
     toast: true,
